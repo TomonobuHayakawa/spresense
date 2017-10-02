ebb907a5b967dd349b123eccc49b4d330abbac21
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2017-10-02 18:08:15 +0900

    Add LibTargets.mk for building each library modules

---
+++ sdk/LibTargets.mk
@@ -0,0 +1,9 @@
+
+SDKLIBS += lib$(DELIM)libasmp$(LIBEXT)
+SDKDEPDIRS += modules$(DELIM)asmp
+
+modules$(DELIM)asmp$(DELIM)libasmp$(LIBEXT): context
+	$(Q) $(MAKE) -C modules$(DELIM)asmp TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" libasmp$(LIBEXT)
+
+lib$(DELIM)libasmp$(LIBEXT): modules$(DELIM)asmp$(DELIM)libasmp$(LIBEXT)
+	$(Q) install modules$(DELIM)asmp$(DELIM)libasmp$(LIBEXT) lib$(DELIM)libasmp$(LIBEXT)
