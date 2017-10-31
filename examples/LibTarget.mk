8918570a63b80f3dfad291b4fecafc37ad7a5620
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2017-10-31 16:51:00 +0900

    Extract library target path

--- examples/LibTarget.mk
+++ examples/LibTarget.mk
@@ -1,11 +1,9 @@
 # XXX
 
-EXAMPLES_DIR := $(SDKDIR)/../examples
-
-$(EXAMPLES_DIR)$(DELIM)libexamples$(LIBEXT): context
+$(SDKDIR)$(DELIM)..$(DELIM)examples$(DELIM)libexamples$(LIBEXT): context
 	$(Q) $(MAKE) -C $(dir $@) TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" $(notdir $@)
 
-lib$(DELIM)libexamples$(LIBEXT): $(EXAMPLES_DIR)$(DELIM)libexamples$(LIBEXT)
+lib$(DELIM)libexamples$(LIBEXT): $(SDKDIR)$(DELIM)..$(DELIM)examples$(DELIM)libexamples$(LIBEXT)
 	$(Q) install $< $@
 
 EXTLIBS += lib$(DELIM)libexamples$(LIBEXT)
