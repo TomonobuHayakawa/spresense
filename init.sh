12594c93d20c82089c69836351ca25e9e46ff6af
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2017-11-06 11:41:43 +0900

    Change checkout to master for all

--- init.sh
+++ init.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-SUBDIRS='sdk examples proprietary'
-
-# Do submodule initialization process anyway
+# Do submodule initialization process anyway.
 # These commands will not be affected if do again and again.
 
 git submodule init
 git submodule update
-
-# Checkout NuttX sources as our branch
-
-(cd nuttx; git checkout sdk2.2) || exit
-(cd apps; git checkout sdk2.2) || exit
-
-# Checkout our sources as master
-
-for d in $SUBDIRS
-do
-    (cd $d; git checkout master) || exit
-done
+git submodule foreach git checkout master
