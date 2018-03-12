62f7ec04c01e047479e6413501e341f08f7ba5ad
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2018-03-12 15:46:55 +0900

    Fix remain object files
    Replace new build structure for fix a bug that remaining compiled object
    files in 'make clean' when option changed.

--- sdk/modules/audio/LibTargets.mk
+++ sdk/modules/audio/LibTargets.mk
@@ -1,4 +1,14 @@
-# Find and include audio library rules
+# Audio Library
 
-include $(wildcard modules/audio/*/LibTargets.mk)
+ifeq ($(CONFIG_SDK_AUDIO),y)
+SDKLIBS += lib$(DELIM)libsdkaudio$(LIBEXT)
+SDKMODDIRS += modules$(DELIM)audio
+endif
+SDKCLEANDIRS += modules$(DELIM)audio
+
+modules$(DELIM)audio$(DELIM)libsdkaudio$(LIBEXT): context
+	$(Q) $(MAKE) -C modules$(DELIM)audio TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" libsdkaudio$(LIBEXT)
+
+lib$(DELIM)libsdkaudio$(LIBEXT): modules$(DELIM)audio$(DELIM)libsdkaudio$(LIBEXT)
+	$(Q) install $< $@
 
