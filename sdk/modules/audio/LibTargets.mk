9318b69acaf50aa09be825e489ffbbb8d585653b
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2018-05-28 19:03:40 +0900

    Fix build error in parallel
    refs #6610

--- sdk/modules/audio/LibTargets.mk
+++ sdk/modules/audio/LibTargets.mk
@@ -10,15 +10,8 @@ SDKCLEANDIRS += modules$(DELIM)audio$(DELIM)dsp$(DELIM)worker$(DELIM)lib
 
 .PHONY: workerbin asmpwlib
 
-modules$(DELIM)audio$(DELIM)libsdkaudio$(LIBEXT): context workerbin
+modules$(DELIM)audio$(DELIM)libsdkaudio$(LIBEXT): context
 	$(Q) $(MAKE) -C modules$(DELIM)audio TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" libsdkaudio$(LIBEXT)
-
-asmpwlib:
-	$(Q) $(MAKE) -C modules$(DELIM)audio$(DELIM)dsp$(DELIM)worker$(DELIM)lib TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" APPDIR="$(APPDIR)" CROSSDEV=$(CROSSDEV)
-
-workerbin: asmpwlib
-	$(Q) $(MAKE) -C modules$(DELIM)audio$(DELIM)dsp$(DELIM)worker TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" APPDIR="$(APPDIR)" CROSSDEV=$(CROSSDEV)
 
 lib$(DELIM)libsdkaudio$(LIBEXT): modules$(DELIM)audio$(DELIM)libsdkaudio$(LIBEXT)
 	$(Q) install $< $@
-
