36d96428db1ea3b81972b51d6246e8e9d8c71e95
Nobuto Kobayashi <Nobuto.Kobayashi@sony.com>
2018-08-15 12:17:05 +0900

    initial commit
    this is tentative

---
+++ examples/LibTarget.mk
@@ -0,0 +1,12 @@
+# XXX
+
+EXAMPLES_DIR := $(SDKDIR)/../examples
+
+$(EXAMPLES_DIR)$(DELIM)libexamples$(LIBEXT): context
+	$(Q) $(MAKE) -C $(dir $@) TOPDIR="$(TOPDIR)" SDKDIR="$(SDKDIR)" $(notdir $@)
+
+lib$(DELIM)libexamples$(LIBEXT): $(EXAMPLES_DIR)$(DELIM)libexamples$(LIBEXT)
+	$(Q) install $< $@
+
+EXTLIBS += lib$(DELIM)libexamples$(LIBEXT)
+EXTSUBDIRS += $(EXAMPLES_DIR)
